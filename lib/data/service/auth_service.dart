651f931a579b6a08e3b901ce06734adc03233e22
mihaigheorghe <mihai.gheorghe@devm8.com>
2024-01-13 17:21:26 +0200

    added device id for request

--- lib/data/service/auth_service.dart
+++ lib/data/service/auth_service.dart
@@ -1,5 +1,7 @@
 import 'dart:developer';
+import 'dart:io';
 
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:pizza_app/data/service/api_service.dart';
 
@@ -8,6 +10,15 @@ class AuthService extends ApiService{
 
   void sendAccountInformation(GoogleSignInAccount account) async{
     var auth = await account.authentication;
+    var deviceInfo = DeviceInfoPlugin();
+    String? deviceId;
+    if (Platform.isIOS) { // import 'dart:io'
+      var iosDeviceInfo = await deviceInfo.iosInfo;
+      deviceId = iosDeviceInfo.identifierForVendor; // unique ID on iOS
+    } else if(Platform.isAndroid) {
+      var androidDeviceInfo = await deviceInfo.androidInfo;
+      deviceId = androidDeviceInfo.id; // unique ID on Android
+    }
     Map<String, String> data = {
       "displayName": account.displayName.toString(),
       "email": account.email,
@@ -15,6 +26,7 @@ class AuthService extends ApiService{
       "serverAuthCode": account.serverAuthCode.toString(),
       "idToken": auth.idToken.toString(),
       "accessToken": auth.accessToken.toString(),
+      "deviceId": deviceId.toString(),
     };
     log(data.toString());
     var response = await dio.post(authUrl, data: data);
